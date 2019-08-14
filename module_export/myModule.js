9c0109ac676188806c8e6b6b688722e99b4172ec
Dhananjay kumar <dhananjay.kumar@neudesic.com>
2019-08-14 23:33:33 +0530

    more samples

--- module_export/myModule.js
+++ module_export/myModule.js
@@ -2,3 +2,6 @@ module.exports = function() {console.log("module/default export.")}
 module.exports.myFunc = function() {console.log("my function.")}
 module.exports.myConst = "my constant.";
 module.exports.MyObject = { abc: "my constant."};
+
+console.log(module);
+console.log(module.exports);
